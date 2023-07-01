32dc5555c54a4e47545b79c52fdd6f8f3d856bac
Soap1196 <Jdmack007@aol.com>
2023-07-01 15:09:32 -0500

    studying mongoDB

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, session, request, url_for, redirect, session
 from functools import wraps
-from pymongo import MongoClient
+import pymongo
 
 app = Flask(__name__)
 from user import routes
@@ -8,10 +8,11 @@ from user import routes
 
 app = Flask(__name__)
 app.secret_key = "testing"
-def MongoDB():
-    client = MongoClient('hostname', 27017)
-    db = client.get_database('total_records')
-    records = db.register
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["CustomerDB"]
+mycol = mydb["CustomerCollection"]
+records = myclient
 
 
 
@@ -25,27 +26,15 @@ def index():
         email = request.form.get("email")
         
         password1 = request.form.get("password1")
-        password2 = request.form.get("password2")
         
-        user_found = records.find_one({"name": user})
-        email_found = records.find_one({"email": email})
-        if user_found:
-            message = 'There already is a user by that name'
-            return render_template('index.html', message=message)
-        if email_found:
-            message = 'This email already exists in database'
-            return render_template('index.html', message=message)
-        if password1 != password2:
-            message = 'Passwords should match!'
-            return render_template('index.html', message=message)
-        else:
-            user_input = {'name': user, 'email': email, 'password': password2}
-            records.insert_one(user_input)
+        
+        user_input = {'name': user, 'email': email, 'password': password1}
+        records.insert_one(user_input)
             
-            user_data = records.find_one({"email": email})
-            new_email = user_data['email']
+        user_data = records.find_one({"email": email})
+        new_email = user_data['email']
    
-            return render_template('logged_in.html', email=new_email)
+        return render_template('logged_in.html', email=new_email)
     return render_template('index.html')
 
 @app.route('/logged_in')
