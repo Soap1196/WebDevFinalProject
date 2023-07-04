9072fc47e9368883fdffa908fffe30f2748609bd
Eric Dollar <ericdapossum@gmail.com>
2023-07-04 14:37:53 -0500

    Venv installed

--- Eric Yummy Template/db/testdb.py
+++ Eric Yummy Template/db/testdb.py
@@ -2,8 +2,14 @@ from flask import Flask, request, render_template, session, redirect
 import pandas as pd
 import numpy as np
 
+# menuitems = {'Item':['Cheese Sticks','Cheese Pizza','Spaghetti','Coca-Cola']}
 
-menuitems = {'Item':['Cheese Sticks','Cheese Pizza','Spaghetti','Coca-Cola']}
+# df=pd.DataFrame(menuitems)
 
-df=pd.DataFrame(menuitems)
-df
+testdb = Flask(__name__, template_folder='Eric Yummy Template')
+@testdb.route('/')
+def index():
+    return render_template("menu.html")
+
+if __name__ == '__main__':
+    testdb.run(debug=True)
