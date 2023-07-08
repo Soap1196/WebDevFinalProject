c1f241f40115f22d84e0309238452e9934278d40
Vira <vira@uab.edu>
2023-07-09 02:10:44 +0530

    add to cart endpoint (work in progress)

--- app.py
+++ app.py
@@ -121,8 +121,32 @@ def foodmenu():
 
     return render_template('menu.html', fullmenu=fullmenu, email=email)
 
+# Add To Cart feature
+# references:
+# - https://stackabuse.com/how-to-get-and-parse-http-post-body-in-flask-json-and-form-data/
+# - https://stackoverflow.com/questions/45412228/sending-json-and-status-code-with-a-flask-response
+@app.route("/add-to-cart", methods=["POST"])
+def add_to_cart():
+    requestJson = request.get_json()
+    print(f"received /add-to-cart request with data: {requestJson}")
 
+    addToCartData = requestJson.get('userEmailAndItemId')
+    if not addToCartData:
+        message = "could not add to cart; userEmailAndItemId not sent"
+        return {"message": message}, 400 # bad request status code!
 
+    addToCartDataSplit = addToCartData.split()
+    if len(addToCartDataSplit) != 2:
+        message = "could not add to cart; userEmailAndItemId invalid"
+        return {"message": message}, 400 # bad request status code!
+
+    userEmail = addToCartDataSplit[0]
+    itemId = addToCartDataSplit[1]
+
+    print(f"adding item {itemId} to cart for user {userEmail}")
+    # TODO: actually add item to the user's cart in the database
+
+    return {"message": "successfully added item to cart"}, 200
 
 if __name__ == "__main__":
   app.run(debug=True)
